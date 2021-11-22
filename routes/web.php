374b75dd54ee2e25b60a771328c2c2cadd2fa0c9
girhan <lusandi.girhan@gmail.com>
2021-11-22 10:47:55 +0700

    Enter

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,20 @@ Route::get('ets','ViewController@showETS');
 Route::get('filephp','ViewController@showFilePHP');
 Route::post('indexphp','ViewController@showIndexPHP');
 
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
 
+//route tambah file
+Route::get('/pegawai/tambah','PegawaiController@tambah');
 
+//route pegawai/store
+Route::post('/pegawai/store','PegawaiController@store');
+
+//route edit
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+//route update
+Route::post('/pegawai/update','PegawaiController@update');
+
+//route hapus
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
